58fa93dd632a833f2ee1ac00807b9695b2248036
Maria Helena Larroza <malenaa2905@gmail.com>
2025-03-11 12:20:36 -0300

    Atualizações

---
+++ 2º Ensino Médio/Projeto 1/aula1.js
@@ -0,0 +1,8 @@
+document.getElementById("meuBotao").addEventListener('click',
+function() {
+    alert('got me looking so crazy right now')
+});
+
+function mudarTexto() {
+    document.getElementById("mensagem").innerHTML = "i got this feeling inside my bones";
+}
