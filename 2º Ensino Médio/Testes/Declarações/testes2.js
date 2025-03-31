af3ce0cca260d024bafb917a7b4459873b7f42b7
Maria Helena Larroza <malenaa2905@gmail.com>
2025-03-31 14:37:14 -0300

    Atualizações

---
+++ 2º Ensino Médio/Testes/Declarações/testes2.js
@@ -0,0 +1,7 @@
+let x, y, z;
+x= 2; y= 4; z= x+y;
+
+document.getElementById("demo").innerHTML = "O valor de z é " + z + ".";
+
+let w = 5; // Vou ser executado
+// w = 6; Não vou ser executado
---
+++ 2º Ensino Médio/Testes/Exibições/testes.js
@@ -0,0 +1,11 @@
+document.getElementById("demo").innerHTML = "<h3>Hey guys!</h3>";
+
+document.getElementById("demo2").innerText = "I love chocolate";
+
+document.write(5 + 6);
+
+window.alert("Hermione Granger");
+
+console.log("Ninfandora Tonks");
+
+window.print()
