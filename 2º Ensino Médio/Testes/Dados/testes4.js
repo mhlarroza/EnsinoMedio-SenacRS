aace84624beb1e2376bb537e1d75201b46e1770c
Maria Helena Larroza <malenaa2905@gmail.com>
2025-05-25 20:16:48 -0300

    Atualizações

---
+++ 2º Ensino Médio/Testes/Dados/testes4.js
@@ -0,0 +1,27 @@
+// Numbers:
+let altura = 16;
+let largura = 7.5;
+
+// Strings:
+let cor = "Rosa";
+let sobrenome = "Larroza";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const pessoa = {nome:"Maria", sobrenome:"Larroza"};
+
+// Array object:
+const carros = ["Porsche", "Lamborguini", "BMW"];
+
+// Date object:
+const data = new Date("2025-04-01");
+
+// números decimais ou extragrandes
+let w = 123e5;    // 12300000
+let z = 123e-5;   // 0.00123
+
+// matriz por meio de array
+const cars = ["Saab", "Volvo", "BMW"]; 
---
+++ 2º Ensino Médio/Testes/testes5.js
@@ -0,0 +1,8 @@
+let video= true;
+
+if (video == true) {
+    console.log("Você terá shopkins de volta!")
+}
+else {
+    console.log("Você não terá o shopkins.")
+}
