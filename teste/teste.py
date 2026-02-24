eddbebd9ade3ac188f23261de6215b136fda096f
Maria Helena Larroza <malenaa2905@gmail.com>
2026-02-24 09:03:36 -0300

    Delete teste directory

--- teste/teste.py
+++
@@ -1,8 +0,0 @@
-from deep_translator import GoogleTranslator
-
-tradutor = GoogleTranslator(source="en", target="pt")
-
-texto = "Hello World!"
-
-traducao = tradutor.translate(texto)
-print(traducao)
